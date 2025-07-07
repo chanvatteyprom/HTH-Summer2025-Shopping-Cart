2a327762f1433630d87295e192dfe8934e6e31c0
Chanvattey <172544644+chanvatteyprom@users.noreply.github.com>
2025-07-07 13:44:41 -0700

    making js

---
+++ shopping-cart.js
@@ -0,0 +1,10 @@
+let shoppingcart = 'Clothings';
+const doesntChange = 'Limited Stocks';
+
+shoppingcart = false; 
+
+console.log(shoppingcart);
+console.log(doesntChange);
+
+const myArray = [1, 2, 3, 'Tops', 'Bottoms', 'Dresses']
+myArray[2] = 7;
